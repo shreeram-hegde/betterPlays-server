d28fc2f0bed48c65c27bc4f1cdcbcad886ecc18e
shreeram-hegde <shreeeramhegde@gmail.com>
2025-10-31 17:39:36 +0530

    checking mongodb

--- configs/db.js
+++ configs/db.js
@@ -8,6 +8,7 @@ const connectDB = async () => {
   try {
     mongoose.connection.on("connected", dbNotifier);
     await mongoose.connect(`${process.env.MONGO_URI}/betterPlays`);
+    console.log("Mongo URI:", process.env.MONGO_URI);
   } catch (error) {
     console.log(error.message);
   }
